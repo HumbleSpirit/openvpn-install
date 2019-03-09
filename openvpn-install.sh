88d9e83d4e4bbeea7e93f8d345f1837311d9369d
HumbleSpirit <borodkin@gmail.com>
2019-03-10 01:36:06 +0200

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -16,63 +16,8 @@ function tunAvailable () {
 }
 
 function checkOS () {
-	if [[ -e /etc/debian_version ]]; then
-		OS="debian"
-		source /etc/os-release
-
-		if [[ "$ID" == "debian" ]]; then
-			if [[ ! $VERSION_ID =~ (8|9) ]]; then
-				echo "⚠️ Your version of Debian is not supported."
-				echo ""
-				echo "However, if you're using Debian >= 9 or unstable/testing then you can continue."
-				echo "Keep in mind they are not supported, though."
-				echo ""
-				until [[ $CONTINUE =~ (y|n) ]]; do
-					read -rp "Continue? [y/n]: " -e CONTINUE
-				done
-				if [[ "$CONTINUE" = "n" ]]; then
-					exit 1
-				fi
-			fi
-		elif [[ "$ID" == "ubuntu" ]];then
-			OS="ubuntu"
-			if [[ ! $VERSION_ID =~ (16.04|18.04) ]]; then
-				echo "⚠️ Your version of Ubuntu is not supported."
-				echo ""
-				echo "However, if you're using Ubuntu > 17 or beta, then you can continue."
-				echo "Keep in mind they are not supported, though."
-				echo ""
-				until [[ $CONTINUE =~ (y|n) ]]; do
-					read -rp "Continue? [y/n]: " -e CONTINUE
-				done
-				if [[ "$CONTINUE" = "n" ]]; then
-					exit 1
-				fi
-			fi
-		fi
-	elif [[ -e /etc/fedora-release ]]; then
-		OS=fedora
-	elif [[ -e /etc/centos-release ]]; then
-		if ! grep -qs "^CentOS Linux release 7" /etc/centos-release; then
-			echo "Your version of CentOS is not supported."
-			echo "The script only support CentOS 7."
-			echo ""
-			unset CONTINUE
-			until [[ $CONTINUE =~ (y|n) ]]; do
-				read -rp "Continue anyway? [y/n]: " -e CONTINUE
-			done
-			if [[ "$CONTINUE" = "n" ]]; then
-				echo "Ok, bye!"
-				exit 1
-			fi
-		fi
-		OS=centos
-	elif [[ -e /etc/arch-release ]]; then
-		OS=arch
-	else
-		echo "Looks like you aren't running this installer on a Debian, Ubuntu, Fedora, CentOS or Arch Linux system"
-		exit 1
-	fi
+	OS=centos
+	
 }
 
 function initialCheck () {
