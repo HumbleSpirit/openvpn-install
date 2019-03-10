044251416b2bc5dcf6873bb1a83d9d24aaf4edce
HumbleSpirit <borodkin@gmail.com>
2019-03-10 11:42:37 +0200

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -896,7 +896,7 @@ function newClient () {
 	echo "Tell me a name for the client."
 	echo "Use one word only, no special characters."
 
-	until [[ "$CLIENT" =~ ^[a-zA-Z0-9_]+$ ]]; do
+	until [[ "$CLIENT" =~ ^[a-zA-Z0-9._]+$ ]]; do
 		read -rp "Client name: " -e CLIENT
 	done
 
