1c75d5d10fb129bf049487874f0d73b39953d92e
HumbleSpirit <borodkin@gmail.com>
2019-05-14 15:03:37 +0300

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -900,7 +900,7 @@ function newClient () {
 #	until [[ "$CLIENT" =~ ^[a-zA-Z0-9._]+$ ]]; do
 #		read -rp "Client name: " -e CLIENT
 #	done
-CLIENT="${UUID}_${PDName}"
+CLIENT="${UUID}"_"${PDName}"
 echo "Client id is " $CLIENT
 #	echo ""
 #	echo "Do you want to protect the configuration file with a password?"
